2871ea042ef44e63650dece4ead53bba0ce01ec7
kiran3311 <kirangangurde.26396@gmail.com>
2025-07-20 15:06:46 +0530

    fix2

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base :  "/RK_Lighting",
+ // base : import.meta.env.VITE_BASE_PATH || "/RK_Lighting",
 });
